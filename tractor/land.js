038d1fce008c962f2a58c244d0fb1b4c68851208
paul hoover <paul@nabbr.com>
2014-03-22 08:08:13 -0400

    tutorial images added to tractor; land class working; beginning of shop scene (not functioning)

--- tractor/land.js
+++ tractor/land.js
@@ -1,13 +1,33 @@
 goog.provide('tractor.Land');
 goog.require('lime.Sprite');
 
-farming.Land = function(gameObj, playerObj) {
-		goog.base(this);
-		this.setAnchorPoint(0, 0);
-		this.setSize(gameObj.tile_size, gameObj.tile_size);
-		this.setFill('images/bare_land.png');
-		
-		
+tractor.Land = function(gameObj, playerObj) {
+
+	goog.base(this);
+	this.setAnchorPoint(0, 0);
+	this.setSize(gameObj.tile_size, gameObj.tile_size);
+	this.setFill('images/bare_land.png');
+	
+	this.state = this.EMPTY;
+
+	var land = this;
+    goog.events.listen(this,['mousedown', 'touchstart'], function(e) {
+        e.event.stopPropagation();        
+        if(land.state == land.EMPTY && playerObj.money >= gameObj.costPlowing) {
+            //plow land
+            land.setFill('images/plowed.png')
+            land.state = land.PLOWED;
+
+            //update player money
+            playerObj.money -= gameObj.costPlowing;
+            gameObj.updateMoney();
+        }
+    });
 }
+
+tractor.Land.prototype.EMPTY = 0;
+tractor.Land.prototype.PLOWED = 1;
+tractor.Land.prototype.GROWING = 2;
+tractor.Land.prototype.READY = 3;
 
 goog.inherits(tractor.Land, lime.Sprite);
--- tractor/tractor.js
+++ tractor/tractor.js
@@ -7,6 +7,7 @@ goog.require('lime.Director');
 goog.require('lime.Scene');
 goog.require('lime.Layer');
 goog.require('lime.GlossyButton'); 
+goog.require('tractor.Land');
 
 // entrypoint
 tractor.start = function(){
@@ -41,12 +42,81 @@ tractor.start = function(){
     gameScene.appendChild(controlsLayer);
 
     director.replaceScene(gameScene); 
+
+	//shop
+	var shopScene = new lime.Scene().setRenderer(lime.Renderer.CANVAS);
+	var shopLayer = new lime.Layer().setAnchorPoint(0, 0);
+
+	var shopBackground = new lime.Sprite().setAnchorPoint(0,0).setPosition(0,0)
+	    .setSize(gameObj.width, gameObj.height).setFill('#0D0D0D');
+	shopLayer.appendChild(shopBackground);
+	shopScene.appendChild(shopLayer);
+
+	//close button
+	var closeButton = new lime.GlossyButton().setColor('#133242').setText('Back')
+	    .setPosition(gameObj.width/2, gameObj.height-25)
+	    .setSize(80, 40);
+	shopLayer.appendChild(closeButton);
+
+	//launch shop event
+	goog.events.listen(shopButton,['mousedown', 'touchstart'], function(e) {
+	    director.replaceScene(shopScene);
+	});
+
+	//close shop event
+	goog.events.listen(closeButton,['mousedown', 'touchstart'], function(e) {
+	    director.replaceScene(gameScene);
+	});
+	
 	//player object
 	var playerObj = {
 	    money: 300,
 	    currentCrop: 0             
 	}
 	
+	gameObj.crops = [
+	    {
+	        name: 'tomato',
+	        cost: 10,
+	        revenue: 18,
+	        time_to_ripe: 10, //secods
+	        time_to_death: 30, //second from when it's ripe
+	        image: 'tomato.png'
+	    },
+	    {
+	        name: 'artichoke',
+	        cost: 20,
+	        revenue: 38,
+	        time_to_ripe: 60,
+	        time_to_death: 60,
+	        image: 'artichoke.png'
+	    },
+	    {
+	        name: 'lettuce',
+	        cost: 15,
+	        revenue: 26,
+	        time_to_ripe: 30,
+	        time_to_death: 60,
+	        image: 'lettuce.png'
+	    },
+	    {
+	        name: 'eggplant',
+	        cost: 30,
+	        revenue: 78,
+	        time_to_ripe: 120,
+	        time_to_death: 120,
+	        image: 'eggplant.png'
+	    },
+	    {
+	        name: 'peppers',
+	        cost: 40,
+	        revenue: 82,
+	        time_to_ripe: 180,
+	        time_to_death: 180,
+	        image: 'peppers.png'
+	    }
+	];
+
 	//controls area
 	var controlArea = new lime.Sprite().setAnchorPoint(0,0)
 	    .setPosition(0, gameObj.height-gameObj.controlsLayer_h)
@@ -65,6 +135,19 @@ tractor.start = function(){
 	    .setPosition(gameObj.controlsLayer_w-50, gameObj.height-gameObj.controlsLayer_h/2);
 	controlsLayer.appendChild(moneyLabel); 
 	
+	//updating money indicator
+	gameObj.updateMoney = function() {
+	    moneyLabel.setText('$'+playerObj.money);
+	};
+
+	
+	//create land elements
+	for(var i=0; i<gameObj.num_tiles_x; i++) {
+	   for(var j=0; j<gameObj.num_tiles_y; j++) {
+	       var landElement = new tractor.Land(gameObj, playerObj).setPosition(i*gameObj.tile_size, j*gameObj.tile_size);
+	       landLayer.appendChild(landElement);
+	   }
+	}
 }
 
 
